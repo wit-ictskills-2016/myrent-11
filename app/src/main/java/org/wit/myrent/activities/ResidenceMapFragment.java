7c879650b161ad600e2ebfeec1222be456a3ea4a
John Fitzgerald <usplitu@hotmail>
2016-10-12 09:10:00 +0100

    ResidenceMapFragment implements GoogleMap.OnMarkerDragListener

--- app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
@@ -3,16 +3,37 @@ package org.wit.myrent.activities;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.Marker;
 
 /**
  * Created by jfitzgerald on 12/10/2016.
  */
-public class ResidenceMapFragment extends MapFragment implements OnMapReadyCallback
+public class ResidenceMapFragment extends MapFragment implements OnMapReadyCallback,
+    GoogleMap.OnMarkerDragListener
 {
 
+  // OnMapReadyCallback
   @Override
   public void onMapReady(GoogleMap googleMap) {
-    
+
+  }
+
+  // GoogleMap.OnMarkerDragListener
+  @Override
+  public void onMarkerDragStart(Marker marker) {
+
+  }
+
+  // GoogleMap.OnMarkerDragListener
+  @Override
+  public void onMarkerDrag(Marker marker) {
+
+  }
+
+  // GoogleMap.OnMarkerDragListener
+  @Override
+  public void onMarkerDragEnd(Marker marker) {
+
   }
 }
 
