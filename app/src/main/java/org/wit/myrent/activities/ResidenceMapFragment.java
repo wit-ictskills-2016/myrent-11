92e0cb7b8b2294a084bbcb8d48e225aecc6ebc0b
John Fitzgerald <usplitu@hotmail>
2016-10-12 09:08:36 +0100

    ResidenceMapFragment implements OnMapReadyCallback

---
+++ app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
@@ -0,0 +1,18 @@
+package org.wit.myrent.activities;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+/**
+ * Created by jfitzgerald on 12/10/2016.
+ */
+public class ResidenceMapFragment extends MapFragment implements OnMapReadyCallback
+{
+
+  @Override
+  public void onMapReady(GoogleMap googleMap) {
+    
+  }
+}
+
