5e281abf5a87b8ff7abdd1ebb8e4d89f0111b480
John Fitzgerald <usplitu@hotmail>
2016-10-12 09:17:34 +0100

    ResidenceMapFragment onCreate & onCreateView wrappers

--- app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
@@ -1,9 +1,18 @@
 package org.wit.myrent.activities;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.Marker;
+
+import org.wit.myrent.R;
+import org.wit.myrent.app.MyRentApp;
+import org.wit.myrent.models.Residence;
 
 /**
  * Created by jfitzgerald on 12/10/2016.
@@ -14,6 +23,29 @@ public class ResidenceMapFragment extends MapFragment implements OnMapReadyCallb
     GoogleMap.OnCameraIdleListener,
     GoogleMap.OnMarkerClickListener
 {
+
+  /*
+  * We use the current residence when navigating back to parent class - ResidenceFragment as
+  * this is required in ResidenceFragment onCreate. The navigateUp
+  */
+  Long resId;
+
+  Residence residence; // The residence associated with this map fragment
+  MyRentApp app;
+  GoogleMap map;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
+    super.onCreateView(inflater, parent, savedInstanceState);
+    View v = inflater.inflate(R.layout.activity_map, parent, false);
+
+    return v;
+  }
 
   // OnMapReadyCallback
   @Override
