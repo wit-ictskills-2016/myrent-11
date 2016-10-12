82b70a981a6a7ec6dbac7069d2f199b15301dcd6
John Fitzgerald <usplitu@hotmail>
2016-10-12 09:27:51 +0100

    ResidenceMapFragment complete implementation of interface methods

--- app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
+++ app/src/main/java/org/wit/myrent/activities/ResidenceMapFragment.java
@@ -6,11 +6,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.wit.android.helpers.MapHelper;
 import org.wit.myrent.R;
 import org.wit.myrent.app.MyRentApp;
 import org.wit.myrent.models.Residence;
@@ -64,11 +68,34 @@ public class ResidenceMapFragment extends MapFragment implements OnMapReadyCallb
       default: return super.onOptionsItemSelected(item);
     }
   }
-  
+
   // OnMapReadyCallback
+
+  /**
+   * Invoked by framework once fragment created
+   * @param googleMap The map associated with this residence
+   */
   @Override
   public void onMapReady(GoogleMap googleMap) {
+    this.map = map;
 
+    map.setOnMarkerDragListener(this);
+    map.setOnInfoWindowClickListener(this);
+    map.setOnMarkerClickListener(this);
+    map.setOnCameraIdleListener(this);
+
+    LatLng latLng = MapHelper.latLng(getActivity(), residence.geolocation);
+    MarkerOptions options = new MarkerOptions()
+        .title("Residence")
+        .snippet("GPS : " + latLng.toString())
+        .draggable(true)
+        .position(latLng);
+
+    map.addMarker(options);
+
+    map.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, (float)residence.zoom));
+
+    map.setOnInfoWindowClickListener(this);
   }
 
   // GoogleMap.OnMarkerDragListener
@@ -84,26 +111,50 @@ public class ResidenceMapFragment extends MapFragment implements OnMapReadyCallb
   }
 
   // GoogleMap.OnMarkerDragListener
+  /**
+   * When marker drag ends, save Residence model geolocation and zoom.
+   * @param marker The map marker representing current residence geolocation.
+   */
   @Override
   public void onMarkerDragEnd(Marker marker) {
-
+    residence.geolocation = MapHelper.latLng(marker.getPosition());
+    residence.zoom = map.getCameraPosition().zoom;
+    map.animateCamera(CameraUpdateFactory.newLatLng(marker.getPosition()));
   }
 
   // GoogleMap.OnInfoWindowClickListener
+  /**
+   * Click an open infowindow to close it.
+   * Click on a marker to open an infowindow
+   * @param marker The marker associated with this infowindow.
+   */
   @Override
   public void onInfoWindowClick(Marker marker) {
-
+    marker.hideInfoWindow();
   }
 
   // GoogleMap.OnCameraIdleListener
+  /**
+   * We implement this interface to capture zoom when marker not dragged but zoom changed
+   * example by pinching screen.
+   * When google map camera stops moving we capture the zoom and save to model.
+   */
   @Override
   public void onCameraIdle() {
-
+    residence.zoom = map.getCameraPosition().zoom;
   }
 
   // GoogleMap.OnMarkerClickListener
+  /**
+   * The purpose of including this method is to update the info window information
+   * with the geolocation of the current position of the marker.
+   * @param marker The marker whose info window being refreshed
+   * @return Returns false indicating default behaviour, i.e. open info window.
+   */
   @Override
   public boolean onMarkerClick(Marker marker) {
+    LatLng latLng = MapHelper.latLng(getActivity(), residence.geolocation);
+    marker.setSnippet("GPS : " + latLng.toString());
     return false;
   }
 }
